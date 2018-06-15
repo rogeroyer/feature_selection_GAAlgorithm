1ab3462dbe4178cda32fc67e27e468a4e7c70c17
roger <rogerluo387@gmail.com>
2018-06-15 22:49:15 +0800

    修复了不能将上一代的最优个体遗传给下一代的bug。

--- Genetic_algorithm.py
+++ Genetic_algorithm.py
@@ -15,9 +15,12 @@ class GA(object):
         self.geneLenght = aGeneLenght  # 基因长度 #
         self.matchFun = aMatchFun  # 适配函数
         self.lives = []  # 种群
-        self.best = None  # 保存这一代中最好的个体
+        # self.best = None  # 保存这一代中最好的个体
+        self.best = Life(np.random.randint(0, 2, self.geneLenght))  # 保存这一代中最好的个体
+
         self.gene = np.random.randint(0, 2, self.geneLenght)  # 保存全局最好的个体 #
         self.score = -1   # 保存全局最高的适应度 #
+
         self.generation = 0  # 第几代 #
         self.crossCount = 0  # 交叉数量 #
         self.mutationCount = 0  # 突变个数 #
@@ -27,16 +30,22 @@ class GA(object):
     def initPopulation(self):
         """初始化种群"""
         self.lives = []
-        for i in range(self.lifeCount):
+        count = 0
+        while count < 30:
             gene = np.random.randint(0, 2, self.geneLenght)
-            random.shuffle(gene)  # 随机洗牌 #
+            if sum(gene) < 40:
+                continue
+            random.shuffle(gene)               # 随机洗牌 #
             life = Life(gene)
             self.lives.append(life)
+            count += 1
 
     def judge(self):
         """评估，计算每一个个体的适配值"""
         self.bounds = 0.0
-        self.best = self.lives[0]
+        # self.best = self.lives[0]
+        self.best.score = copy.deepcopy(self.score)  ####
+        self.best.gene = copy.deepcopy(self.gene)  ####
         for life in self.lives:
             life.score = self.matchFun(life)
             self.bounds += life.score
@@ -47,13 +56,12 @@ class GA(object):
             self.score = copy.deepcopy(self.best.score)           ####
             self.gene = copy.deepcopy(self.best.gene)             ####
 
-        self.best.score = copy.deepcopy(self.score)               ####
-        self.best.gene = copy.deepcopy(self.gene)                 ####
+        # self.best.score = copy.deepcopy(self.score)               ####
+        # self.best.gene = copy.deepcopy(self.gene)                 ####
 
     def cross(self, parent1, parent2):
         """
         函数功能：交叉
-        函数实现：随机交叉长度为n的片段，n为随机产生
         """
         index1 = random.randint(0, self.geneLenght - 1)  # 随机生成突变起始位置 #
         index2 = random.randint(index1, self.geneLenght - 1)  # 随机生成突变终止位置 #
@@ -109,9 +117,9 @@ class GA(object):
         """产生下一代"""
         self.judge()
         newLives = []
-        # newLives.append(self.best)  # 把最好的个体加入下一代 #
-        newLives.append(self.best)  # 把最好的个体加入下一代 #                 ####
-
+        newLives.append(self.best)  # 把最好的个体加入下一代 #
+        newLives[0].gene = copy.deepcopy(self.gene)
+        newLives[0].score = copy.deepcopy(self.score)
         while len(newLives) < self.lifeCount:
             newLives.append(self.newChild())
         self.lives = newLives
